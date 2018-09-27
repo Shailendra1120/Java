ab6039e32a28d8abd428709709e44d072419d821
Shailendra Chauhan <41744293+Shailendra1120@users.noreply.github.com>
2018-09-27 15:43:36 +0530

    do while practise.java

---
+++ do while.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class loop{
+public static void main(String[]args){
+int n;
+Scanner input = new Scanner(System.in);
+
+int i=0;
+do{
+System.out.println("i is. "+i);
+i++;
+}
+while(i<5);
+}
+}
